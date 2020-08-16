eef03f3692038513123992e72e31eed8f20d1390
Ewan Tindale <ewannnnn@gmail.com>
2020-08-16 18:49:05 +0100

    make navbar opaque on scroll

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Navbar() {
+  const [position, setPosition] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  function handleScroll() {
+    setPosition(window.pageYOffset);
+  }
   return (
-    <div className="navbar">
+    <div className={position > 20 ? "navbar scrolled" : "navbar"}>
       <img
         src="/images/netflix.png"
         className="netflix-logo"
